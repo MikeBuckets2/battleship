b68982ee3539b01e3e0a6c2d1e97bc74bbcbd8f4
Michael Morris <michaelmorris121@yahoo.com>
2026-02-05 14:42:58 -0600

    feat: ship class that tracks ship length hits and if sunk

---
+++ src/ship.test.js
@@ -0,0 +1,24 @@
+import { Ship } from './ship.js';
+
+test('ship has a length', () => {
+    const ship = new Ship(3);
+    expect(ship.length).toBe(3);
+});
+
+test('ship starts with 0 hits', () => {
+    const ship = new Ship(3);
+    expect(ship.hits).toBe(0);
+});
+
+test('hit() increases hits', () => {
+    const ship = new Ship(3);
+    ship.hit();
+    expect(ship.hits).toBe(1);
+});
+
+test('ship is sunk when hits equal length', () => {
+    const ship = new Ship(2);
+    ship.hit();
+    ship.hit();
+    expect(ship.isSunk()).toBe(true);
+});
